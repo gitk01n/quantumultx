6f3e65b1ac178702c1d03536ebb9d7d070d0df28
gitk01n <83260491+gitk01n@users.noreply.github.com>
2025-07-18 00:24:55 +0800

    Update miqilin.js

--- script/miqilin.js
+++ script/miqilin.js
@@ -19,7 +19,7 @@ ck有效期只有半小时,每周一获取ck后手动运行脚本
 
 [Script]
 cron "0 15 13 * * 1" script-path=https://raw.githubusercontent.com/gitk01n/quantumultx/refs/heads/main/script/miqilin.js, timeout=300, tag=米其林会员俱乐部
-http-request ^https:\/\/ulp\.michelin\.com\.cn\/bff\/profile script-path=https://gist.githubusercontent.com/Sliverkiss/49c5d5176cad6e47919ffe058606ed0d/raw/michelin.js, timeout=10, tag=米其林俱乐部token
+http-request ^https:\/\/ulp\.michelin\.com\.cn\/bff\/profile script-path=https://raw.githubusercontent.com/gitk01n/quantumultx/refs/heads/main/script/miqilin.js, timeout=10, tag=米其林俱乐部token
 
 [Mitm]
  hostname=ulp.michelin.com.cn 
